73cbd78b46b41446f36eb4887f261be85f74943f
lc6261 <46859216+lc6261@users.noreply.github.com>
2021-12-12 19:09:09 +0800

    del
    123

--- tls加密转发/server/1.sh
+++
@@ -1,2 +0,0 @@
-nohup ./ghostunnel server --listen 0.0.0.0:1445 --target en.huobipool.com:1800 --keystore server.pem --cacert ca.pem --allow-cn client --unsafe-target
- > /dev/null 2>&1 &
--- tls加密转发/客户端/pool.bat
+++
@@ -1,1 +0,0 @@
-ghostunnel-v1.6.0-windows-amd64.exe client --listen 0.0.0.0:8007 --target 182.61.148.232:14401 --unsafe-listen --keystore client.pem --cacert ca.pem 
